c8079c1085e62e0d970f4a2fbb45901eee97fa8d
Paul Morris <pmorris2012@fau.edu>
2021-11-09 19:26:37 -0500

    delete images that could not be retrieved

---
+++ validate.py
@@ -0,0 +1,26 @@
+from pandas import read_csv
+import typer
+
+from pathlib import Path
+from typing import Optional
+
+
+def validate(
+    save_dir: Optional[Path] = typer.Argument("mushroom_images/"),
+    tsv: Optional[Path] = typer.Option("mushrooms.tsv.gz", "--tsv")
+    ):
+    data = read_csv(tsv, sep="\t", header=0)
+
+    drop_rows = []
+    for index, row in data.iterrows():
+        path = Path(save_dir, row['filename'])
+        if not path.is_file():
+            drop_rows.append(index)
+            
+    data = data.drop(labels=drop_rows, axis=0)
+
+    data.to_csv(str(tsv), index=False, compression='gzip')
+
+
+if __name__ == "__main__":
+    typer.run(download)
